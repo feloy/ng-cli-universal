d7c282556af2dd3b3cb993d366fa6a03bb08c7a7
pmartin <contact@elol.fr>
2017-07-30 09:51:12 +0200

    simple app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'page1', component: Page1Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
